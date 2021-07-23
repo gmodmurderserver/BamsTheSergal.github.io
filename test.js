f6653479dce365f1f84212dc359516cb8429a0ea
Bamsy <12705993+BamsTheSergal@users.noreply.github.com>
2021-07-23 09:39:11 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,20 @@
 <script>
 function myFunction() {
   // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
   filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
 
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
     }
   }
 }
