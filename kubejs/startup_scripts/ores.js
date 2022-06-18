18554b8045b76096019e421c58d54324ff7bdb94
gmodmurderserver <89837032+gmodmurderserver@users.noreply.github.com>
2022-06-18 08:37:31 -0500

    Update ores.js

--- kubejs/startup_scripts/ores.js
+++ kubejs/startup_scripts/ores.js
@@ -2,7 +2,7 @@ onEvent('block.registry', event => {
   event.create('scandium_ore')
   	   .material('stone')
        .hardness(1)
-       .displayName('Scandium Ore') // No longer required in 1.18.2+
-       .tagBlock('minecraft:mineable/pickaxe') // Make it mine faster using a shovel in 1.18.2+
+       .displayName('Scandium Ore')
+       .tagBlock('minecraft:mineable/pickaxe')
 	   
 })
