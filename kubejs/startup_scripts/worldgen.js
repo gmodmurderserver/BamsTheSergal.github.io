c37d4c0a0ce260d9c70c66e7809db4f1fa56e8de
gmodmurderserver <89837032+gmodmurderserver@users.noreply.github.com>
2022-06-18 08:22:05 -0500

    Add files via upload

--- kubejs/startup_scripts/worldgen.js
+++ kubejs/startup_scripts/worldgen.js
@@ -5,7 +5,7 @@ onEvent('worldgen.add', event => {
 		ore.addTarget('minecraft:stone', 'kubejs:scandium_ore')
 		ore.count([1, 8])
 			.squared()
-			.triangleHeight(50, 32)
+			.triangleHeight(25, 8)
 	})
 onEvent('worldgen.remove', event => {
   event.removeOres(ores => {
