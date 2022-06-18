cd2b97c25a65f7fb75244a2e964848769a11c064
gmodmurderserver <89837032+gmodmurderserver@users.noreply.github.com>
2022-06-18 07:04:18 -0500

    Add files via upload

---
+++ kubejs/startup_scripts/worldgen.js
@@ -0,0 +1,14 @@
+onEvent('worldgen.add', event => {
+//scandium test	
+  event.addOre(ore => {
+    ore.block = 'kubejs:scandium_ore' // Block ID (Use [] syntax for properties)
+    ore.spawnsIn.values = [ // List of valid block IDs or tags that the ore can spawn in
+      '#minecraft:base_stone_overworld' // Default behavior - ores spawn in all stone types
+    ]
+    ore.clusterMaxSize = 8 // Max blocks per cluster
+    ore.clusterCount = 20 // Clusters per chunk
+    ore.minHeight = -56 // Min Y ore spawns in
+    ore.maxHeight = -32 // Max Y ore spawns in
+    ore.squared = true // Adds random value to X and Z between 0 and 16. Recommended to be true
+  })
+})
