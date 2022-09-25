4b6f1cddd9befca224caff7938ceab039e149e7c
gmodmurderserver <89837032+gmodmurderserver@users.noreply.github.com>
2022-09-25 10:18:47 -0500

    Delete kubejs directory

--- kubejs/startup_scripts/ores.js
+++
@@ -1,8 +0,0 @@
-onEvent('block.registry', event => {
-  event.create('scandium_ore')
-  	   .material('stone')
-       .hardness(1)
-       .displayName('Scandium Ore')
-       .tagBlock('minecraft:mineable/pickaxe')
-	   
-})
